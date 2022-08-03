98b87ab64e414563e4f07c41e2e5924b29fc22cf
Jairo Acosta <jairoacosta_@hotmail.com>
2022-08-02 22:16:10 -0500

    primer homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,12 +1,22 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
+  let decimal=0;
+  for (let i=0;i<num.length;i++){
+    decimal+=num[num.length-1-i]*2**i;
+  }
+  return decimal;
 
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+  let binario=[];
+  while(num>1){
+    binario.push(num%2);
+    num=Math.floor(num/2);
+  }
+  binario.push(num);
+  return binario.reverse().join("");
 
 }
 
